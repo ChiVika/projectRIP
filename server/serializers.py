355947591b20b0214f2cd4c94b7ba6ab8dda8d00
ChiVika <animevita03@mail.ru>
2024-09-21 20:01:53 +0700

    API(CRUD)

---
+++ server/serializers.py
@@ -0,0 +1,9 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import RecipePost
+
+
+class PostSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = RecipePost
+        fields = "__all__"
--- server/urls.py
+++ server/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import views
+from .views.views import PostsViews, PostViewsId
 
 urlpatterns = [
-    path('', views.getRoutes, name="routes"),
-    path('api/proba/', views.proba1_list, name="proba_list"),
+    # path('', views.getRoutes, name="routes"),
+    # path('createPost/', views.create_post, name="createPost"),
+    # path('api/proba/', views.proba1_list, name="proba_list"),
+    path("api/", PostsViews.as_view(), name="API"),
+    path("api/<int:ids>/", PostViewsId.as_view(), name="detail_API")
+
 
 ]
