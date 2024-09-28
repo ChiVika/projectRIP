959bc0f0a93d3bc11efc019f0cec33f0b302921a
ChiVika <animevita03@mail.ru>
2024-09-28 16:10:41 +0700

    Изображения к посту

--- server/urls.py
+++ server/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views.views import PostsViews, PostViewsId
+from .views.views import PostsViews, PostViewsId, PostDatas
+from django.urls import include
+from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
+
+router = DefaultRouter()
+router.register('', PostDatas)
 
 urlpatterns = [
-    # path('', views.getRoutes, name="routes"),
-    # path('createPost/', views.create_post, name="createPost"),
-    # path('api/proba/', views.proba1_list, name="proba_list"),
     path("api/", PostsViews.as_view(), name="API"),
-    path("api/<int:ids>/", PostViewsId.as_view(), name="detail_API")
-
-
-]
+    path("api/<int:ids>/", PostViewsId.as_view(), name="detail_API"),
+    path("api/blog/", include(router.urls))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
