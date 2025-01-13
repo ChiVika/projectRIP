24d289dda6c277faba63f578cc3889fef284d714
ChiVika <animevita03@mail.ru>
2025-01-13 19:17:44 +0700

    настроила localstorage и docker с nginx проблемы

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -3,12 +3,14 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import MyProfile from '../components/profile';
+import AddNotes from '../components/Notes';
 
 function Profile({user}) {
 
     return (
         <>
         <MyProfile user={user}/>
+        <AddNotes/>
         </>
     );
     }
