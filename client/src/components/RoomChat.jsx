7d664d0fd670efde7f5a255397afc8c20186bab5
an.vokirok <korikovaanastasia780@gmail.com>
2025-01-13 23:52:01 +0700

    стили карточек и чата

--- client/src/components/RoomChat.jsx
+++ client/src/components/RoomChat.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './RoomChat.css'; // Импортируйте стили
 
 function ChatRoom() {
   const [messages, setMessages] = useState([]);
@@ -39,14 +40,12 @@ function ChatRoom() {
     }
   };
 
-
-
   return (
-    <div>
+    <div className="chat-container">
       <h1>Общий чат</h1>
       <textarea id="chat-log" cols="100" rows="20" value={messages.join('\n')} readOnly></textarea>
       <br />
-      <input id="chat-message-input" type="text" size="100" value={messageInput}onChange={(e) => setMessageInput(e.target.value)}/>
+      <input id="chat-message-input" type="text" size="100" value={messageInput} onChange={(e) => setMessageInput(e.target.value)} />
       <br />
       <button id="chat-message-submit" onClick={handleSendMessage} disabled={!isConnected}>
         Отправить
